9042d45407ec12141f4ecf4754c5be617f994ca3
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-16 21:00:30 +1100

    Initial commit

---
+++ display_the_ip_address_of_the_machine/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * C# Program to Display the IP Address of the Machine
+ */
+using System;
+using System.Net;
+
+namespace display_the_ip_address_of_the_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = string.Empty; //getting the Host Name.
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);// Using Host Name,IP address is obtained.
+            IPAddress[] addr = ipEntry.AddressList;
+
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {1} : ", addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
